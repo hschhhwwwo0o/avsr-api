93324b86155f0b708758d59e12263d8baa3ea157
Saveliy <ssandry.github@gmail.com>
2022-04-10 16:46:44 +0300

    Init dotenv

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const logger = require("./logger");
+require("dotenv").config();
 
 module.exports = function (app) {
-  mongoose.connect(app.get("mongodb")).catch(err => {
+  mongoose.connect(String(process.env.MONGODB)).catch(err => {
     logger.error(err);
     process.exit(1);
   });
