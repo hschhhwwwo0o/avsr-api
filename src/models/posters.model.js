22600593132081b1ecc4f43391f5d7fe4b18d979
Saveliy <ssandry.github@gmail.com>
2022-04-10 17:43:59 +0300

    Posters model

--- src/models/posters.model.js
+++ src/models/posters.model.js
@@ -4,7 +4,9 @@ module.exports = function (app) {
   const { Schema } = mongooseClient;
   const schema = new Schema(
     {
-      text: { type: String, required: true },
+      title: { type: String, required: true },
+      image: { type: String, required: true },
+      authorId: { type: String, required: true },
     },
     {
       timestamps: true,
